46f5e834103cb5cf68d7059254dfc8a495485417
hohukel <okuyama1625@gmail.com>
2020-02-16 15:39:07 +0900

    :sparkles: Add REGION and ZONE

--- export-env.sh
+++ export-env.sh
@@ -1,4 +1,7 @@
-export PROJECT=pokoyakazan-test-01
+#export PROJECT=pokoyakazan-test-01
+export PROJECT=$(gcloud config get-value project)
 export BUCKET=pokoyakazan-test-01
+export REGION=us-central1
+export ZONE=us-central1-a
 export INSTANCE_NAME=flights-instance
 export IP=$(curl http://ipecho.net/plain)
