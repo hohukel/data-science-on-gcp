543369a293c1db01519737b15746b7fd3fa879c0
hohukel <okuyama1625@gmail.com>
2020-02-21 08:23:02 +0900

    :rocket: Change GCP Project!!

--- export-env.sh
+++ export-env.sh
@@ -1,8 +1,7 @@
-#export PROJECT=pokoyakazan-test-01
 export PROJECT=$(gcloud config get-value project)
-export BUCKET=pokoyakazan-test-01
+export BUCKET=hohukelkazan
 export REGION=us-central1
 export ZONE=us-central1-a
-export INSTANCE_NAME=flights-instance
+export CLOUD_SQL_INSTANCE_NAME=flights-instance
 export DATA_PROC_CLUSTER=ch6cluster
 export IP=$(curl http://ipecho.net/plain)
