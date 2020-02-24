cb8a6101de3180ad0de3227d658745f414e428cd
hohukel <okuyama1625@gmail.com>
2020-02-24 23:33:07 +0900

    :tada: Incomplete setup file causing error that cannot be executed with python3

--- 06_dataproc/create_datalab_cluster.sh
+++ 06_dataproc/create_datalab_cluster.sh
@@ -8,29 +8,21 @@ echo "BUCKET=$BUCKET"
 echo "REGION=$REGION"
 echo "ZONE=$ZONE"
 echo "DATA_PROC_CLUSTER=$DATA_PROC_CLUSTER"
-echo "DATALAB_INIT"=gs://dataproc-initialization-actions/datalab/datalab.sh
+echo "CONDA_ENV_NAME=$CONDA_ENV_NAME"
 
 echo "Upload install file"
 sed "s/CHANGE_TO_USER_NAME/$USER/g" install_on_cluster.sh > /tmp/install_on_cluster.sh
 gsutil cp /tmp/install_on_cluster.sh $INSTALL
 
 echo "Create cluster"
-#gcloud dataproc clusters create $DATA_PROC_CLUSTER \
-    #--region $REGION \
-    #--zone=$ZONE \
-    #--num-workers=2 \
-    #--worker-machine-type=n1-standard-2 \
-    #--master-machine-type=n1-standard-2 \
-    #--metadata 'CONDA_PACKAGES="python==3.5"' \
-    #--scopes cloud-platform \
-    #--initialization-actions gs://goog-dataproc-initialization-actions-${REGION}/conda/bootstrap-conda.sh,gs://goog-dataproc-initialization-actions-${REGION}/conda/install-conda-env.sh,gs://goog-dataproc-initialization-actions-${REGION}/datalab/datalab.sh,$INSTALL
-
 gcloud dataproc clusters create $DATA_PROC_CLUSTER \
     --project $PROJECT \
     --region $REGION \
     --zone $ZONE \
     --num-workers=2 \
-    --worker-machine-type=n1-standard-2 \
     --master-machine-type=n1-standard-2 \
-    --initialization-actions \
-        gs://dataproc-initialization-actions/datalab/datalab.sh,$INSTALL
+    --worker-machine-type=n1-standard-2 \
+    --master-boot-disk-size=15GB \
+    --worker-boot-disk-size=15GB \
+    --scopes cloud-platform \
+    --initialization-actions $INSTALL,gs://goog-dataproc-initialization-actions-${REGION}/datalab/datalab.sh
--- 06_dataproc/install_on_cluster.sh
+++ 06_dataproc/install_on_cluster.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+set -exo pipefail
+PROFILE_SCRIPT_PATH=/etc/profile.d/effective-python.sh
+
+
+echo "alias python='/usr/bin/python3.5'" | tee -a "${PROFILE_SCRIPT_PATH}" /etc/environment /usr/lib/spark/conf/spark-env.sh /etc/*bashrc /etc/profile /etc/profile.d/spark_config.sh
+
+echo "spark.executorEnv.PYTHONHASHSEED=0" >> /etc/spark/conf/spark-defaults.conf /etc/profile.d/spark_config.sh
+
+echo "export PYSPARK_PYTHON=/usr/bin/python3.5" | tee -a "${PROFILE_SCRIPT_PATH}" /etc/environment /usr/lib/spark/conf/spark-env.sh /etc/*bashrc /etc/profile /etc/profile.d/spark_config.sh
+
+# CloudSDK libraries are installed in system python
+echo 'export CLOUDSDK_PYTHON=/usr/bin/python3.5' | tee -a "${PROFILE_SCRIPT_PATH}" /etc/environment /usr/lib/spark/conf/spark-env.sh /etc/*bashrc /etc/profile /etc/profile.d/spark_config.sh
+
+source ${PROFILE_SCRIPT_PATH}
+source /etc/environment
+source /usr/lib/spark/conf/spark-env.sh
+source /etc/*bashrc
+source /etc/profile
+
 # install Google Python client on all nodes
 apt-get update
 apt-get install -y python-pip
 pip install --upgrade google-api-python-client
-
-conda install nb_conda_kernels
-conda create --quiet --yes -n py35 python=3.5 ipykernel
-source activate py35
-export PYSPARK_PYTHON=python3.5
-
 
 # git clone on Master
 USER=CHANGE_TO_USER_NAME
@@ -17,6 +30,7 @@ ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 
 if [[ "${ROLE}"=='MASTER' ]];then
   cd /home/$USER
-  #git clone https://github.com/GoogleCloudPlatform/data-science-on-gcp.git
   git clone https://github.com/hohukel/etl-on-gcp.git
 fi
+
+
