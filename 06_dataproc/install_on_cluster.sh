ba7db1270406244d45aa562749e75dc63e5181a3
hohukel <okuyama1625@gmail.com>
2020-02-24 10:42:39 +0900

    :tada: Add debug

--- 06_dataproc/install_on_cluster.sh
+++ 06_dataproc/install_on_cluster.sh
@@ -5,12 +5,18 @@ apt-get update
 apt-get install -y python-pip
 pip install --upgrade google-api-python-client
 
+conda install nb_conda_kernels
+conda create --quiet --yes -n py35 python=3.5 ipykernel
+source activate py35
+export PYSPARK_PYTHON=python3.5
+
+
 # git clone on Master
 USER=CHANGE_TO_USER_NAME
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 
 if [[ "${ROLE}"=='MASTER' ]];then
-  cd home/$USER
-  # git clone https://github.com/GoogleCloudPlatform/data-science-on-gcp.git
-  git clone https://github.com/GoogleCloudPlatform/data-science-on-gcp.git
+  cd /home/$USER
+  #git clone https://github.com/GoogleCloudPlatform/data-science-on-gcp.git
+  git clone https://github.com/hohukel/etl-on-gcp.git
 fi
