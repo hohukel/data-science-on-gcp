318f6011d4e77ccd57369b13decf5c8ed835c7a0
hohukel <okuyama1625@gmail.com>
2020-02-24 10:41:03 +0900

    :shower: Unuse script creating conda env *using python2*

--- 06_dataproc/create_datalab_cluster.sh
+++ 06_dataproc/create_datalab_cluster.sh
@@ -7,6 +7,7 @@ echo "PROJECT=$PROJECT"
 echo "BUCKET=$BUCKET"
 echo "REGION=$REGION"
 echo "ZONE=$ZONE"
+echo "DATA_PROC_CLUSTER=$DATA_PROC_CLUSTER"
 echo "DATALAB_INIT"=gs://dataproc-initialization-actions/datalab/datalab.sh
 
 echo "Upload install file"
@@ -14,11 +15,22 @@ sed "s/CHANGE_TO_USER_NAME/$USER/g" install_on_cluster.sh > /tmp/install_on_clus
 gsutil cp /tmp/install_on_cluster.sh $INSTALL
 
 echo "Create cluster"
+#gcloud dataproc clusters create $DATA_PROC_CLUSTER \
+    #--region $REGION \
+    #--zone=$ZONE \
+    #--num-workers=2 \
+    #--worker-machine-type=n1-standard-2 \
+    #--master-machine-type=n1-standard-2 \
+    #--metadata 'CONDA_PACKAGES="python==3.5"' \
+    #--scopes cloud-platform \
+    #--initialization-actions gs://goog-dataproc-initialization-actions-${REGION}/conda/bootstrap-conda.sh,gs://goog-dataproc-initialization-actions-${REGION}/conda/install-conda-env.sh,gs://goog-dataproc-initialization-actions-${REGION}/datalab/datalab.sh,$INSTALL
+
 gcloud dataproc clusters create $DATA_PROC_CLUSTER \
-  --region=$REGION \
-  --num-workers=2 \
-  --scopes=cloud-platform \
-  --worker-machine-type=n1-standard-2 \
-  --master-machine-type=n1-standard-2 \
-  --zone=us-central1-a \
-  --initialization-actions=$INSTALL,gs://goog-dataproc-initialization-actions-${REGION}/conda/bootstrap-conda.sh,gs://goog-dataproc-initialization-actions-${REGION}/conda/install-conda-env.sh,gs://goog-dataproc-initialization-actions-${REGION}/datalab/datalab.sh \
+    --project $PROJECT \
+    --region $REGION \
+    --zone $ZONE \
+    --num-workers=2 \
+    --worker-machine-type=n1-standard-2 \
+    --master-machine-type=n1-standard-2 \
+    --initialization-actions \
+        gs://dataproc-initialization-actions/datalab/datalab.sh,$INSTALL
